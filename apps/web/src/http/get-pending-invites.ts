b0822a9c337672d7e99af73e30cc935fb764a0a8
Danrlei <danrleidalfre@gmail.com>
2024-08-09 13:00:41 -0300

    add accept and reject invite logged in

---
+++ apps/web/src/http/get-pending-invites.ts
@@ -0,0 +1,24 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetPendingInvitesResponse {
+  invites: {
+    organization: {
+      name: string
+    }
+    id: string
+    role: Role
+    email: string
+    createdAt: string
+    author: {
+      id: string
+      name: string | null
+      avatarUrl: string | null
+    } | null
+  }[]
+}
+
+export async function getPendingInvites() {
+  return await api.get('pending-invites').json<GetPendingInvitesResponse>()
+}
